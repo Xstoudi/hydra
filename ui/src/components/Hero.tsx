9404a48e0aa0dd2cc1400ae9d95eb23fa95ca1e8
Xavier Stouder <xavier@stouder.io>
2022-10-29 20:01:22 +0200

    fix: add hover effect on hero button

--- ui/src/components/Hero.tsx
+++ ui/src/components/Hero.tsx
@@ -16,7 +16,7 @@ function Hero() {
         <div className='flex h-full w-full flex-col items-center justify-center  gap-4 text-white'>
           <h1 className='text-6xl'>Hydra</h1>
           <p className='text-center text-2xl'>{t('description')}</p>
-          <Link to='/stations' className='rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 py-4 px-8 text-xl'>{t('enter-app-button')}</Link>
+          <Link to='/stations' className='rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 py-4 px-8 text-xl hover:bg-gradient-to-br'>{t('enter-app-button')}</Link>
         </div>
       </div>
     </header>
