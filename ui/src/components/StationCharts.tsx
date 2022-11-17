c81acd74f75656ecd6e4f640486651efa73e5e80
Xavier Stouder <xavier@stouder.io>
2022-11-17 22:25:17 +0100

    fix: remove test throw

--- ui/src/components/StationCharts.tsx
+++ ui/src/components/StationCharts.tsx
@@ -32,7 +32,6 @@ interface StationChartsProps {
 }
 
 export default function StationCharts({ stationId }: StationChartsProps) {
-  throw new Error('test')
   const { data: seriesData, isLoading: areSeriesLoading } = useQuery(
     ['station', stationId, 'measures'],
     () => getMeasures(stationId),
