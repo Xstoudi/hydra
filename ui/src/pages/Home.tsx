04df10f8488e10761758cbf960501b5d320e080e
Xavier Stouder <xavier@stouder.io>
2022-12-20 18:24:08 +0100

    feat: improve homepage

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,11 +1,35 @@
 import { memo } from 'react'
+import { useTranslation } from 'react-i18next'
+import Footer from '../components/common/Footer'
 import Hero from '../components/Hero'
 
 function Home() {
+
+  const { t } = useTranslation('home')
+
   return (
-    <>
+    <div className='flex flex-col gap-8'>
       <Hero />
-    </>
+      <div className='grid grid-cols-1 md:grid-cols-2 px-20 lg:px-40 xl:px-60 2xl:px-80 gap-8 xl:gap-12 2xl:gap-x-16'>
+        <div>
+          <h2 className='text-3xl'>{t('parts.realtime.title')}</h2>
+          <p className='text-lg text-justify'>{t('parts.realtime.description')}</p>
+        </div>
+        <div>
+          <h2 className='text-3xl'>{t('parts.opensource.title')}</h2>
+          <p className='text-lg text-justify'>{t('parts.opensource.description')}</p>
+        </div>
+        <div>
+          <h2 className='text-3xl'>{t('parts.unverified.title')}</h2>
+          <p className='text-lg text-justify'>{t('parts.unverified.description')}</p>
+        </div>
+        <div>
+          <h2 className='text-3xl'>{t('parts.responsive.title')}</h2>
+          <p className='text-lg text-justify'>{t('parts.responsive.description')}</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
