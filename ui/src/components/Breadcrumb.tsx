ed068eba9a7b2957eaa84338177c6278be5ce089
Xavier Stouder <xavier@stouder.io>
2022-12-20 17:40:11 +0100

    fix: improve responsiveness

--- ui/src/components/Breadcrumb.tsx
+++ ui/src/components/Breadcrumb.tsx
@@ -4,15 +4,15 @@ import useBreadcrumbs from '../hooks/use-breadcrumb'
 export default function Breadcrumb() {
   const { breadcrumbs } = useBreadcrumbs()
   
-  return <>
+  return <div className='hidden sm:block'>
     {
       breadcrumbs.map(breadcrumb => (
     
-        <Link key={breadcrumb.path} to={breadcrumb.path} className='text-gray-500 hover:text-gray-700 text-xl leading-10 ml-2 mt-1'>
+        <Link key={breadcrumb.path} to={breadcrumb.path} className='text-gray-500 hover:text-gray-700 text-xl leading-10 ml-2 mt-1 inline-block'>
           {'>'} {breadcrumb.label}
         </Link>
     
       ))
     }
-  </>
+  </div>
 }
