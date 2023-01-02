000fed8347363dcf0e40bd2e0b710ffeb14908e8
Xavier Stouder <xavier@stouder.io>
2023-01-02 17:06:07 +0100

    feat: add pwa config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'hydra.svg'],
+      manifest: {
+        name: 'Hydra',
+        short_name: 'Hydra',
+        description: 'Swiss hydrological data',
+        theme_color: '#3b82f6',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ]
+      }
+    })
+  ],
 })
